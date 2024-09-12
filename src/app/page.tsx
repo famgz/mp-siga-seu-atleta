8834f1145e58884eb8f70324d89eb6267a7ade07
famgz <famgz@proton.me>
2024-09-12 11:35:26 -0300

    perf: wrap requests within promise.all

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import AthleteList from '@/components/athlete-list';
 import Filters from '@/components/filters';
-import { findAthletes } from '@/services/athlete';
+import { findAthletes, getCategoriesCount } from '@/services/athlete';
 import { findSports } from '@/services/sports';
 import { Category } from '@/types/sport';
 import { Suspense } from 'react';
@@ -9,22 +9,34 @@ interface Props {
   searchParams: {
     q?: string;
     category?: Category;
+    sport?: string;
   };
 }
 
 export default async function Home({ searchParams }: Props) {
   const searchText = searchParams?.q || '';
   const category = searchParams?.category || 'all';
-  const athletes = await findAthletes({
-    searchText,
-    category,
-  });
-  const sports = await findSports();
+  const sportCode = searchParams?.sport;
+
+  const [athletes, categoriesCount, sports] = await Promise.all([
+    findAthletes({
+      searchText,
+      category,
+      sportCode,
+    }),
+    getCategoriesCount({
+      searchText,
+      sportCode,
+    }),
+    findSports({ searchText }),
+  ]);
 
   return (
     <main className='p-4 flex flex-col gap-12'>
-      <Filters />
-      <Suspense key={searchText + category} fallback={<div>Carregando...</div>}>
+      <Filters sports={sports} categoriesCount={categoriesCount} />
+      <Suspense
+        key={searchText + category + sportCode}
+        fallback={<div>Carregando...</div>}>
         <AthleteList
           filters={{ searchText, category }}
           initialData={athletes}
